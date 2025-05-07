4795ef49afb3351c05d7f1fb720a18eb9ba2483e
Atixmega <kharimoff927@gmail.com>
2025-05-07 23:28:11 +0400

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="bg-black text-yellow-400 min-h-screen flex flex-col justify-center items-center">
+      <h1 className="text-3xl font-bold">Multi-Currency Wallet Demo</h1>
+      <p className="mt-4">Tam işlək sistem hazırlanır...</p>
+    </div>
+  );
+}
+
+export default App;
